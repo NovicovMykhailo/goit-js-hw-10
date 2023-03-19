8e5ab366a53f1f35bd59a31c70e3ba4b2c80630f
NovicovMykhailo <mikenovicov@gmail.com>
2023-03-19 11:52:40 +0200

    create project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import fetchcountries from './fetchCountries'
+import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
+
+
+const DEBOUNCE_DELAY = 300;
