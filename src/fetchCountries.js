c5d1db2ba150a59c544d9ea5c7dc95fcb5cc2125
NovicovMykhailo <mikenovicov@gmail.com>
2023-03-19 14:47:51 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,19 @@
-export {fetchCountries}
+export { fetchCountries };
 
-function fetchCountries(name){
-    console.log(name)
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let obj = data.map(e => {
+        const nameOfficial = e.name.official;
+        const capital = e.capital;
+        const population = e.population;
+        const flag = e.coatOfArms.svg;
+        const language = Object.values(e.languages).toString().split(',').join(', ');
+        return { nameOfficial, capital, population, flag, language };
+      });
+      return obj
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import './css/styles.css';
-import fetchcountries from './fetchCountries'
+import { fetchCountries } from './fetchCountries.js';
 import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
 
-
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputField: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfoCard: document.querySelector('.country-info'),
+};
+
+refs.inputField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+  let inputValue = refs.inputField.value.toLowerCase().trim();
+
+  fetchCountries(inputValue).then(e =>
+
+    e.map(el => {
+    // console.log('onInput -> el', el)
+        
+      let { nameOfficial, capital, population, flag, language } = el;
+      if(flag === undefined){
+        flag = `https://gdr.one/simg/200x250/ebf2fa/4E4E4E?text=Not%20found`
+      }
+      refs.countryInfoCard.insertAdjacentHTML(
+        'beforeend',
+        `<ul class ="card">
+            <div><img src = "${flag}" alt="Flag" width="200" height="250"/></div>
+                <ul>
+                    <li><p>Повна назва країни:</p>${nameOfficial}</li>
+                    <li><p>Cтолиця:</p> ${capital}</li>
+                    <li><p>Населення:</p> ${population} людей</li>
+                    <li><p>Мови:</p> ${language}</li>
+                </ul>
+        </ul>`
+      );
+    }),
+  );
+}
